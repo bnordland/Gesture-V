3b609aecc2d1090f12d05355b3054cf46889c987
Brian Nordland <bnordland@gmail.com>
2017-04-19 08:40:48 -0400

    Initial Makefile and Main

---
+++ Vehicle/AVR/main.c
@@ -0,0 +1,27 @@
+/************************************************************************
+* FILENAME: main.c                                                      *
+*                                                                       *
+* DESCRIPTION: Gesture-V main                                           *
+*              This is the entry point into the program and contains    *
+*              the necessary functionality of Gesture-V A* Controller   *
+*                                                                       *
+* LICENSE: The MIT License (MIT)                                        *
+*          Copyright (c) 2017 Brian Nordland                            *
+*                                                                       *
+* AUTHOR:  Brian Nordland                                               *
+*                                                                       *
+* --------------------------------------------------------------------  *
+* | Change  | Date     |            |                                 | *
+* | Flag    | (DDMYY)  | Author     | Description                     | *
+* |---------|----------|------------|---------------------------------  *
+* | None    | 18Apr17  | BNordland  | Initial creation                | *
+*  -------------------------------------------------------------------  *
+*************************************************************************/
+
+int main(void)
+{
+    while(1)
+    {
+
+    }
+}
